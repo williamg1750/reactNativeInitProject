a6dd3e89e97b7f7f6b254eaa8b1d31497087b9df
william <william.guan12@gmail.com>
2021-12-08 15:21:02 -0500

    added on press

--- App.js
+++ App.js
@@ -4,13 +4,16 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoal, setCourseGoal] = useState('');
 
   const inputHandler = (enteredText) => {
-    setEnteredGoal(enterText);
+    setEnteredGoal(enteredText);
   };
 
-  const addGoalHandler = (enteredGoal) => {
-    console.log(enteredGoal);
+  const addGoalHandler = () => {
+    //when we press that button. Now a tiny side note, when working with set course goals, course goals here is our previous state and the way React updates the state,this should always be your most current state snapshot but it's not 100% guaranteed, to have that 100% guarantee, when you update your state based on the old state, you can use the function form of this set function where you don't pass the value of your new state here but instead you pass it in a function, typically an anonymous function where you get your current state
+    // setCourseGoal([...courseGoal, enteredGoal]);
+    setCourseGoal((currentGoals) => [...courseGoal, enteredGoal]);
   };
   return (
     <View style={styles.screen}>
@@ -20,8 +23,9 @@ export default function App() {
           style={styles.textInput}
           onChangeText={inputHandler}
         />
-        <Button title='Add' />
+        <Button title='Add' onPress={addGoalHandler} />
       </View>
+      <View></View>
     </View>
   );
 }
