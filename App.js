e7f4954bf282fad888bac97f33b2e1189233728f
william <william.guan12@gmail.com>
2022-01-03 14:12:14 -0500

    flat view and also scroll view

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList,
+} from 'react-native';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
-  const [courseGoal, setCourseGoal] = useState('');
+  const [courseGoals, setCourseGoal] = useState([]);
 
   const inputHandler = (enteredText) => {
     setEnteredGoal(enteredText);
@@ -12,8 +20,12 @@ export default function App() {
 
   const addGoalHandler = () => {
     //when we press that button. Now a tiny side note, when working with set course goals, course goals here is our previous state and the way React updates the state,this should always be your most current state snapshot but it's not 100% guaranteed, to have that 100% guarantee, when you update your state based on the old state, you can use the function form of this set function where you don't pass the value of your new state here but instead you pass it in a function, typically an anonymous function where you get your current state
-    // setCourseGoal([...courseGoal, enteredGoal]);
-    setCourseGoal((currentGoals) => [...courseGoal, enteredGoal]);
+    // setCourseGoal([...courseGoal, enteredGoal]); <-- not Guarantee to update
+    // setCourseGoal((currentGoals) => [...courseGoals, enteredGoal]); <--- not suitable anymore when you are using FlatList
+    setCourseGoal((currentGoals) => [
+      ...courseGoals,
+      { key: Math.random().toString(), value: enteredGoal },
+    ]);
   };
   return (
     <View style={styles.screen}>
@@ -25,7 +37,20 @@ export default function App() {
         />
         <Button title='Add' onPress={addGoalHandler} />
       </View>
-      <View></View>
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={(itemData) => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
+      {/* {courseGoals.map((curGoal) => (
+        <View key={curGoal} style={styles.listItem}>
+          <Text>{curGoal}</Text>
+        </View>
+      ))} */}
     </View>
   );
 }
@@ -43,5 +68,12 @@ const styles = StyleSheet.create({
     borderBottomColor: 'black',
     borderBottomWidth: 1,
     width: 200,
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1,
   },
 });
